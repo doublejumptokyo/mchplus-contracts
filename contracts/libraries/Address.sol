d9f4c0b7241aa65c9d93d8bc969a469b31af85b8
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-10-27 21:43:11 +0900

    Add libraries

---
+++ contracts/libraries/Address.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+library Address {
+
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    function toAscii(address account) internal pure returns (string memory) {
+        bytes32 value = bytes32(uint256(account));
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(42);
+        str[0] = '0';
+        str[1] = 'x';
+        for (uint i = 0; i < 20; i++) {
+            str[2+i*2] = alphabet[uint(uint8(value[i + 12] >> 4))];
+            str[3+i*2] = alphabet[uint(uint8(value[i + 12] & 0x0f))];
+        }
+        return string(str);
+    }
+}

ded5b14e330c4e8848b9ce6b5ad34921ddce47a3
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-11-28 18:40:05 +0900

    Add lib

--- contracts/libraries/String.sol
+++ contracts/libraries/String.sol
@@ -2,6 +2,21 @@ pragma solidity ^0.5.0;
 
 library String {
 
+    function compare(string memory _a, string memory _b) public pure returns (bool) {
+        return (keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b)));
+    }
+
+    function cut(string memory _s, uint256 _from, uint256 _range) public pure returns (string memory) {
+        bytes memory s = bytes(_s);
+        require(s.length >= _from + _range, "_s length must be longer than _from + _range");
+        bytes memory ret = new bytes(_range);
+
+        for (uint256 i = 0; i < _range; i++) {
+            ret[i] = s[_from+i];
+        }
+        return string(ret);
+    }
+
     function concat(string memory _a, string memory _b) internal pure returns (string memory) {
         bytes memory a = bytes(_a);
         bytes memory b = bytes(_b);

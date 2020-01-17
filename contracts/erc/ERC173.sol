e9df9a2a414c6c302964febee46ac3f6bd687b0d
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-01-17 13:16:43 +0900

    Ownerable

---
+++ contracts/erc/ERC173.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.0;
+
+import "./ERC165.sol";
+
+/// @title ERC-173 Contract Ownership Standard
+/// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-173.md
+///  Note: the ERC-165 identifier for this interface is 0x7f5828d0
+interface IERC173 /* is ERC165 */ {
+    /// @dev This emits when ownership of a contract changes.
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /// @notice Get the address of the owner
+    /// @return The address of the owner.
+    function owner() external view returns (address);
+
+    /// @notice Set the address of the new owner of the contract
+    /// @param _newOwner The address of the new owner of the contract
+    function transferOwnership(address _newOwner) external;
+}
+
+contract ERC173 is IERC173, ERC165  {
+    address private _owner;
+
+    constructor() public {
+        _registerInterface(0x7f5828d0);
+        transferOwnership(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner(), "Must be owner");
+        _;
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner() {
+        _transferOwnership(_newOwner);
+    }
+
+    function _transferOwnership(address _newOwner) internal {
+        address previousOwner = owner();
+	_owner = _newOwner;
+        emit OwnershipTransferred(previousOwner, _newOwner);
+    }
+}
--- contracts/roles/Operatable.sol
+++ contracts/roles/Operatable.sol
@@ -1,19 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "./Roles.sol";
-
-interface ERC173 /* is ERC165 */ {
-    /// @dev This emits when ownership of a contract changes.    
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /// @notice Get the address of the owner    
-    /// @return The address of the owner.
-    function owner() external view returns (address);
-	
-    /// @notice Set the address of the new owner of the contract   
-    /// @param _newOwner The address of the new owner of the contract    
-    function transferOwnership(address _newOwner) external;	
-}
+import "../erc/ERC173.sol";
 
 contract Operatable is ERC173 {
     using Roles for Roles.Role;
@@ -26,17 +14,10 @@ contract Operatable is ERC173 {
 
     bool private _paused;
     Roles.Role private operators;
-    address private _owner;
 
     constructor() public {
         operators.add(msg.sender);
-        transferOwnership(msg.sender);
         _paused = false;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner(), "Must be owner");
-        _;
     }
 
     modifier onlyOperator() {
@@ -54,14 +35,8 @@ contract Operatable is ERC173 {
         _;
     }
 
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    function transferOwnership(address _newOwner) public onlyOperator(){
-        address previousOwner = owner();
-        _owner = _newOwner;
-        emit OwnershipTransferred(previousOwner, _newOwner);
+    function transferOwnership(address _newOwner) public onlyOperator() {
+        _transferOwnership(_newOwner);
     }
 
     function isOperator(address account) public view returns (bool) {
